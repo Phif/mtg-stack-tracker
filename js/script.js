d805c523c8f4b196037be164743f30ec07ff83f7
Phif <71464584+Phif@users.noreply.github.com>
2023-07-31 19:30:39 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,18 +6,6 @@ import initBaseIcons from './initBaseIcons.js';
 
 const cardSearch = new CardSearch();
 
-// let resolveAllButton = document.getElementById('resolve-stack-button');
-// resolveAllButton.onclick = () => {
-//     if (window.confirm("Resolve all cards in the stack?")) {
-//         let cardContainer = document.getElementById('card-container');
-//         let cards = cardContainer.querySelectorAll('.card');
-//         for (let i = 0; i < cards.length; i++) {
-//             cards[i].querySelector('.resolve-card-button').click();
-
-//         }
-//     }
-// }
-
 let emptyStackButton = document.getElementById('empty-stack-button');
 emptyStackButton.onclick = () => {
     if (window.confirm("Remove all cards from the stack?")) {
